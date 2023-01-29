6c3592a6d158b9d0b8bd0f754ed6929bd2a12181
Patryk <patryk.ciwinski@gmail.com>
2023-01-29 15:07:29 +0100

    spotify api - some improvements

--- playlist_generator/main.py
+++ playlist_generator/main.py
@@ -1,9 +1,18 @@
+import click
+
 from api.spotify import Spotify
 
 
-spotify = Spotify()
-spotify.connect()
-songs = spotify.search("Kawałek do tańca")
-sample_track_id = songs.tracks.items[0].item_id
-features = spotify.get_tracks_audio_features(sample_track_id)
-print(features)
+@click.command()
+@click.argument("song")
+def main(song):
+    spotify = Spotify()
+    spotify.connect()
+    songs = spotify.search(song)
+    for song in songs:
+        sample_track_id = songs.tracks.items[0].item_id
+        features = spotify.get_tracks_audio_features(sample_track_id)
+        print(features)
+
+if __name__ == "__main__":
+    main()
